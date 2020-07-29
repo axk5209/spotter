700686a738bb21963095918608b331a5174bf468
Arham Kothari <35877732+axk5209@users.noreply.github.com>
2020-07-29 15:43:18 -0500

    Only displaying keypoints given distance threshold

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ const posesCollection = [
   [],
   []
 ]
+const polishedCollection = [
+  [],
+  []
+]
 let complete = false
 const posesCollector = (filename) => {
   return (sketch) => {
@@ -24,24 +28,58 @@ const posesCollector = (filename) => {
       video.volume(0)
       video.onended(function() {
         playing = false
-        if (index === 1)
-        {
+        if (index === 1) {
           complete = true
-          console.log(posesCollection)
-          let vector1 = posesCollection[0][0].poses.map(item => [item.x, item.y]).flatten()
-          let vector2 = posesCollection[1][0].poses.map(item => [item.x, item.y]).flatten()
-            console.log(vector1)
-            console.log(vector2)
-          
-//           let myp5Complete = new p5(
-//           sketchCreator('PoseNetTest1A.mp4'), 
-//           document.getElementById('p5sketch')
-//           );
 
-//           let myp5Complete2 = new p5( 
-//             sketchCreator('PoseNetTest1B.mp4'),
-//             document.getElementById('p5sketch2')
-//           );
+          let polished1 = posesCollection[0]
+          let polished2 = posesCollection[1]
+          let minLength = posesCollection[0].length
+          if (posesCollection[0].length < posesCollection[1].length) {
+            polished2 = posesCollection[1].slice(0, posesCollection[0].length)
+          }
+          if (posesCollection[0].length > posesCollection[1].length) {
+            polished1 = posesCollection[0].slice(0, posesCollection[1].length)
+            minLength = posesCollection[1].length
+          }
+          console.log(polished1.length)
+          for (let i = 0; i < minLength; i++) {
+            let vector1 = polished1[i].poses.map(item => [item.x, item.y]).flatten()
+            let vector2 = polished2[i].poses.map(item => [item.x, item.y]).flatten()
+            let distance = sketch.distance(vector1, vector2)
+
+            polishedCollection[0].push({
+              distance,
+              time: polished1[i].time,
+              poses: polished1[i].poses
+            })
+            polishedCollection[1].push({
+              distance,
+              time: polished2[i].time,
+              poses: polished2[i].poses
+            })
+
+          }
+          console.log(polishedCollection)
+          //           console.log(polished1)
+          //           console.log(polished2)
+          //           let vector1 = posesCollection[0][0].poses.map(item => [item.x, item.y]).flatten()
+          //           let vector2 = posesCollection[1][0].poses.map(item => [item.x, item.y]).flatten()
+          //             console.log(vector1)
+          //             console.log(vector2)
+
+          //           console.log(sketch.cosineSim(vector1, vector2))
+          //           console.log(sketch.distance(vector1, vector2))
+
+
+          let myp5Complete = new p5(
+            sketchCreator('PoseNetTest1A.mp4'),
+            document.getElementById('p5sketch')
+          );
+
+          let myp5Complete2 = new p5(
+            sketchCreator('PoseNetTest1B.mp4'),
+            document.getElementById('p5sketch2')
+          );
         }
       })
       video.addCue(0.0, function() {
@@ -65,11 +103,43 @@ const posesCollector = (filename) => {
           time: video.time(),
           poses: poses[0].pose.keypoints
             .map(item => ({
-              score: item.score, 
-              x: item.position.x, 
-              y: item.position.y}))
+              score: item.score,
+              x: item.position.x,
+              y: item.position.y
+            }))
         })
       }
+    }
+
+    sketch.cosineSim = (A, B) => {
+      let dotproduct = 0;
+      let mA = 0;
+      let mB = 0;
+      for (i = 0; i < A.length; i++) { // here you missed the i++
+        dotproduct += (A[i] * B[i]);
+        mA += (A[i] * A[i]);
+        mB += (B[i] * B[i]);
+      }
+      mA = Math.sqrt(mA);
+      mB = Math.sqrt(mB);
+      let similarity = (dotproduct) / ((mA) * (mB)) // here you needed extra brackets
+      return similarity;
+    }
+
+    sketch.distance = (A, B) => {
+      let dotproduct = 0;
+      let mA = 0;
+      let mB = 0;
+      for (i = 0; i < A.length; i++) { // here you missed the i++
+        dotproduct += (A[i] * B[i]);
+        mA += (A[i] * A[i]);
+        mB += (B[i] * B[i]);
+      }
+      mA = Math.sqrt(mA);
+      mB = Math.sqrt(mB);
+      let similarity = (dotproduct) / ((mA) * (mB)) // here you needed extra brackets
+      let distance = 2 * (1 - similarity);
+      return Math.sqrt(distance);
     }
   }
 }
@@ -98,15 +168,22 @@ const sketchCreator = (filename) => {
       video.hide();
     }
 
-    sketch.onLoad = () => { 
+    sketch.onLoad = () => {
       video.size(400, 400);
       video.volume(0)
       video.speed(0.25)
       video.play()
       video.addCue(0.0, function() {})
-      posesCollection[index].forEach(pose => {
+      polishedCollection[index].forEach(pose => {
         video.addCue(pose.time, function() {
-          keypoints = pose.poses
+          if (pose.distance > 0.04)
+          {
+            keypoints = pose.poses
+          }
+          else 
+          {
+            keypoints = []
+          }
         })
       })
     }
@@ -120,10 +197,9 @@ const sketchCreator = (filename) => {
     }
 
     sketch.drawKeypoints = () => {
-      for (let j = 0; j < keypoints.length; j++) {        
+      for (let j = 0; j < keypoints.length; j++) {
         let keypoint = keypoints[j];
-        if (keypoint.score > 0.1)
-        {
+        if (keypoint.score > 0.1) {
           sketch.fill(255, 0, 0);
           sketch.noStroke();
           sketch.ellipse(keypoint.x, keypoint.y, 10, 10);
@@ -132,5 +208,3 @@ const sketchCreator = (filename) => {
     }
   }
 }
-
-
